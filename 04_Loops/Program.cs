d77928a86687789dee959bfc3e2b51b6250beff7
Yahya Yıldız <79065268+Mirliva58@users.noreply.github.com>
2024-10-27 13:16:00 +0300

    Add files via upload

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region for döngüsü (ders4 başlangıç)
+            //Donguler 1+2+3+4+5+6
+            //3+3=6+4=10+5=15+6=21
+            //For(x;y; z)
+            //x:başlangıç
+            //y:bitiş
+            //z:artış-azalış
+            //int i;
+            //for (i = 1; i <= 5; i++)
+            //    Console.WriteLine("C# Eğitim Kampı");
+
+
+            #endregion
+            #region sınav sorusu (ders4 sonu)
+            //Console.Write("Sayıyı Giriniz: ");
+            //int number = int.Parse(Console.ReadLine());
+            //int ones, tens, hundreds;
+            //int sum;
+            //ones = number % 10;
+            //tens = (number % 100) / 10;
+            //hundreds = number / 100;//4.56 -- > 4
+            //Console.WriteLine(ones + "-" + tens + "-" + hundreds);
+            //sum = ones + tens + hundreds;
+            //Console.WriteLine(sum);
+            #endregion
+        }
+    }
+}
