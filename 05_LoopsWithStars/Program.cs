7f92069cdccf0699f1ac71a3fd256faed305d767
Yahya Yıldız <79065268+Mirliva58@users.noreply.github.com>
2024-10-27 13:17:50 +0300

    Add files via upload

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region yıldızlarla eşkenar dörtgen (ders5 başlangıç ve son)
+            //Console.Write("Kaç satır olsun (tek sayı): ");
+            //int n = int.Parse(Console.ReadLine());
+            //if (n % 2 == 0)
+            //{
+            //    Console.WriteLine("Lütfen tek sayı girin.");
+            //    return;
+            //}
+            //int midpoint = n / 2;
+            //// Üst kısım
+            //for (int i = 0; i <= midpoint; i++)
+            //{
+            //    // Boşluklar
+            //    for (int j = 0; j < midpoint - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    // Yıldızlar
+            //    for (int j = 0; j < 2 * i + 1; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //// Alt kısım
+            //for (int i = midpoint - 1; i >= 0; i--)
+            //{
+            //    // Boşluklar
+            //    for (int j = 0; j < midpoint - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    // Yıldızlar
+            //    for (int j = 0; j < 2 * i + 1; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+        }
+    }
+}
